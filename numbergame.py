8a5c0d5c17726d1260a95ef6cee4327c69d82787
michaeliyaomolere <mikoa04@gmail.com>
2019-03-11 17:53:37 -0400

    Initial commit

---
+++ numbergame.py
@@ -0,0 +1,18 @@
+import random
+
+number = random.randint(1, 10)
+tries = 1
+
+user_name = input("Hi, What is your username")
+
+print("Hi", user_name + ".",)
+
+question = input("Would you like to play a game? [Y/N]")
+if question == "n":
+    print("Have a nice day")
+
+
+if question == "y":
+    print("I'm thinking of a number between 1 and 10")
+    guess = int(input("Guess the number:"))
+    if guess >
